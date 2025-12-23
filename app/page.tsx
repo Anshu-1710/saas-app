707d86eec3fc6d85918a3a5df11f3bd1da598bc7
Anshu Shukla <210305105002@paruluniversity.ac.in>
2025-12-23 19:03:29 +0530

    design the layout of the card not implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className='text-2xl underline'>
-      <h1>Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id = "123"
+          name = "Neura the Brainy explorer"
+          topic = "Neural Network of the Brain"
+          subject = "science"
+          duration = {45}
+          color = "#E5D0FF"
+        />
+        <CompanionCard
+          id = "456"
+          name = "Countsy the Number Wizard"
+          topic = "Derivatives & Integrals"
+          subject = "sMaths"
+          duration = {30}
+          color = "#ffda6e"
+        /><CompanionCard
+          id = "789"
+          name = "Verba the Vocabulary Builder"
+          topic = "English Literature"
+          subject = "Language"
+          duration = {30}
+          color = "#BDE7FF"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
